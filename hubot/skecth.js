e8c85ce4b70369c1fe250b9657ff408810aa5392
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-03-29 21:38:29 +0700

    Create skecth.js
    Sketch of sketch

---
+++ hubot/skecth.js
@@ -0,0 +1,48 @@
+let hubotIo = createElement('ioHubot');
+// const hubot = createElement("hubotio");
+// const botLcd = createElement("botLcd");
+// botLcd.style.height = "10px";
+// botLcd.style.width = "10px";
+// const arms = createElement("arms");
+// const forky = createElement("fork");
+//
+// if(forky) {
+//  forky.addEventListener('click', arms);
+// } else {
+//  console.error = "forky not found";
+// }
+// let forked = "";
+//
+// function hubothubot {
+//  if(hubot === hubotio) {
+//  setInterval((hubotio) => {
+//  let forkfork = input.value;
+//  input.value = "";
+//  forkfork.innerText = forky;
+
+// forky.style.transform=`translate(${fork[0] - 10})px`;
+// }, 30000); //30 second times
+//  }
+//  .catch(err => console.error("error indentify Hubot"));
+// };
+//
+// function arms(clientX, clientY) {
+//  const arm = document.getElementById("arms")
+// arm.style.color = "gray"; //hubot color
+// arm.style.fontSize = "\n 0"; //0
+//
+//  if(forky >= 10) {
+//   const arms = ((clientX / botLcd) * 15) - 10;
+//   const arms = ((clientY / botLcd) * 15) - 10;
+//   alert("push");
+//   console.log("pushed");
+//  } else if (forky <= 10 || forky != 10) {
+//   alert("mona goes run!");
+//   console.log("canceled");
+//  };
+// }
+//
+// document.addEventListener('touchmove', (e) => {
+//  const forking = e.forkingforking[0];
+//  arms(forking.clientX, forking.clientY);
+// });
