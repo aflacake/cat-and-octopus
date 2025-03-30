a4566b5799a766acf9ad1a1dbb78dfb793a37f01
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-03-30 17:22:27 +0700

    Create lcd.js
    lcd of head fbc1272be3b5575565c53930d29d599717f719e0

---
+++ hubot/lcd.js
@@ -0,0 +1,15 @@
+for (let i = 0; i < 5; i++) {
+  const lcd = document.createElement('div');
+    console.log(lcd); //show element available
+  div.classList.add('crt');
+  crt.textContent = `lcd ${i + 1}Hello World`;
+  container.appendChild('lcd');
+}
+
+
+//style
+lcd.style.width = window.innerWidth * 0.10 + 'px';
+lcd.style.height = '50%';
+lcd.style.backgroundColor = "black"; //lcd layer
+crt.style.color = "blue"; //textContent = "Hello World";
+  lcd.style.boder = "100% solid yellow"; //skinColor
