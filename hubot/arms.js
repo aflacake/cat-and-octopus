f098888cb98aef3cca2d54ec33f8fff432c94fb2
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-04-06 08:27:56 +0700

    Update arms.js
    Bg color arms and correction function

--- hubot/arms.js
+++ hubot/arms.js
@@ -1,12 +1,17 @@
+// style
+document.createElement('.div');
+armsVar.style.backgroundCokor = yellow;
+
 var armsVar = 2;
 const armsVarVar = [0]; // scored for pull request
 
 function arms(event) {
-  armsVar += armsVarVar;
-  armsVarVar == armsVar;
+  armsVar += armsVarVar[0];
   let variHubot = 2;
   
   if (variHubot === armsVar) {
-    !(event.armsVar(0)) = true;
+    if (typeof event.armsVar === "function") {
+      event.armsVar(0);
+    }
   }
 }
